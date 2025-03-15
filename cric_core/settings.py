92c6f99b988716284eca79635ee2d8c4029f5926
Bhanu Angam <bhn4477@gmail.com>
2025-03-15 19:04:01 +0100

    Update database configuration to use PostgreSQL; uncomment relevant settings

--- cric_core/settings.py
+++ cric_core/settings.py
@@ -97,28 +97,28 @@ TEMPLATES = [
 WSGI_APPLICATION = "cric_core.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+# # Database
+# # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv("databasename"),
-#         'USER': os.getenv("username"),
-#         'PASSWORD': os.getenv("password"),
-#         'HOST': os.getenv("hostname"),
-#         'PORT': os.getenv("port"),
-#         'OPTIONS': {
-#             'options': '-c search_path=django_schema,public'
-#         }
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("databasename"),
+        'USER': os.getenv("username"),
+        'PASSWORD': os.getenv("password"),
+        'HOST': os.getenv("hostname"),
+        'PORT': os.getenv("port"),
+        'OPTIONS': {
+            'options': '-c search_path=django_schema,public'
+        }
+    }
+}
 
 
 
